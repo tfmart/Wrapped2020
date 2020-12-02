f7d355a77af4120cb655c65fc27c7ca5cc1bae99
Tomas Martins <tomfmart@gmail.com>
2020-12-02 18:58:30 -0300

    Initial Commit

---
+++ Wrapped2020/Wrapped2020App.swift
@@ -0,0 +1,17 @@
+//
+//  Wrapped2020App.swift
+//  Wrapped2020
+//
+//  Created by Tomas Martins on 02/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Wrapped2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
